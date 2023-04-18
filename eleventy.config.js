84eb7848d6f1fce44b15fcf141d3b8767cb0dc17
Daniel <danielhpavey@gmail.com>
2023-04-18 15:59:46 +0000

    Add netlify cms

--- eleventy.config.js
+++ eleventy.config.js
@@ -13,7 +13,10 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/",
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
-	});
+	})
+
+	eleventyConfig.addPassthroughCopy("admin/");
+
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
