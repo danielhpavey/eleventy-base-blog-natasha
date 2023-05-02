a95be3a067ec88a68c42a399adab75f033dde8e1
Daniel <danielhpavey@gmail.com>
2023-05-02 15:29:37 +0000

    Fiddles

--- eleventy.config.js
+++ eleventy.config.js
@@ -13,7 +13,7 @@ module.exports = function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
-		// "./public/": "/",
+		"./public/images/": "images",
 		"./content/img/": "img"
 	})
 
